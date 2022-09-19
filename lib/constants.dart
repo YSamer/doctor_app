6bd135d4169f7a10460f8a88c29693f7cac50457
YSamer <ysamer2525@gmail.com>
2022-09-19 21:59:27 +0200

    first commit

---
+++ lib/constants.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const Color primaryColor = Color(0xFF255ED6);
+const Color textColor = Color(0xFF35364F);
+const Color backgroundColor = Color(0xFFE6EFF9);
+const Color redColor = Color(0xFFE85050);
+
+const double defaultPadding = 16.0;
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:doctor_app/screens/welcome/welcome_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Doctor App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const WelcomeScreen(),
+    );
+  }
+}
